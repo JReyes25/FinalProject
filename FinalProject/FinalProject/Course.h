b6684057461bdedc900eb213b0a8c55a8f87260a
Jorge Reyes <jorgeraul07@hotmail.com>
2020-07-16 17:50:15 -0500

    Init. classes

---
+++ FinalProject/FinalProject/Course.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Course {
+public:
+	Course();
+
+private:
+	string title;
+	string description;
+	int credit;
+	string courseNumber;
+	string semester;
+};
